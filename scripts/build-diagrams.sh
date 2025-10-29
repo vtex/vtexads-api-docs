4bef356ed693dc4b8e3da96a110344fc8a426d5c
Allan Batista <allan@allanbatista.com.br>
2025-10-29 17:34:50 -0300

    update integration disagram

--- scripts/build-diagrams.sh
+++ scripts/build-diagrams.sh
@@ -5,5 +5,5 @@ for file in diagrams/mermaid/*.mmd; do
   filename=$(basename -- "$file")
   extension="${filename##*.}"
   filename="${filename%.*}"
-  npx -p @mermaid-js/mermaid-cli mmdc -i "diagrams/mermaid/$filename.mmd" -o "diagrams/images/$filename.png" -b transparent -p /tmp/puppeteer-config.json
+  npx -p @mermaid-js/mermaid-cli mmdc -i "diagrams/mermaid/$filename.mmd" -o "diagrams/images/$filename.png" -s 4 -b transparent -p /tmp/puppeteer-config.json
 done
